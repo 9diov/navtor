b99be6ae482ba56e1dc398510c27038bc08f9185
Thanh Dinh Khac <thanhdk@gmail.com>
2019-08-09 15:14:54 +0700

    Remove all state mutation from UI class

--- lib/navtor/ui.rb
+++ lib/navtor/ui.rb
@@ -3,28 +3,48 @@ require 'navtor/value'
 module Navtor
   class FileManager; end
 
-  class UI < Value.new(file_manager: FileManager)
+  class UIState < Value.new(:lines, :cols, :offset, :start_line, :end_line, :current_line)
+    def page_size
+      end_line - start_line
+    end
+
+    def reset
+      self.merge(current_line: 0, offset: 0)
+    end
+  end
+
+  class UI
     attr_accessor :start_line, :end_line
 
-    def _initialize(file_manager)
-      Curses.noecho
-      Curses.nonl
-      Curses.stdscr.keypad(true)
-      Curses.raw
-      Curses.stdscr.nodelay = 1
-      self.init_screen
-      @lines = Curses.lines
-      @cols = Curses.cols
-      @offset = 0 # Offset within current directory's entries
-
-      @start_line = 0 # Start line of file list on screen, should be 0
-      @end_line = @lines - 1 # End line of file list on screen
-      @current_line = 0
+    def initialize(file_manager)
+      @file_manager = file_manager
+      init_ui
+      init_screen
+      @state = UIState.with(
+        lines: Curses.lines,
+        cols: Curses.cols,
+        offset: 0, # Offset within current directory's entries
+        start_line: 0, # Start line of file list on screen, should be 0
+        end_line: Curses.lines - 1, # End line of file list on screen
+        current_line: 0
+      )
 
       @input = nil
       stdscr.timeout = -1
       stdscr.scrollok(false) # Avoid scrolling
       stdscr.idlok(true)
+    end
+
+    def reset
+      @state = @state.reset
+    end
+
+    def init_ui
+      Curses.noecho
+      Curses.nonl
+      Curses.stdscr.keypad(true)
+      Curses.raw
+      Curses.stdscr.nodelay = 1
     end
 
     def init_screen
@@ -40,21 +60,12 @@ module Navtor
       Curses.close_screen
     end
 
-    def calculate_lines
-      @end_line = [@lines - 2, @file_manager.entries.size-1].min
-    end
-
-    def page_size
-      @end_line - @start_line
+    def calculate_lines(entries)
+      @state = @state.merge(end_line: [@state.lines - 2, entries.size-1].min)
     end
 
     def stdscr
       Curses.stdscr
-    end
-
-    def reset
-      @current_line = 0
-      @offset = 0
     end
 
     def refresh
@@ -62,11 +73,13 @@ module Navtor
       stdscr.refresh
     end
 
-    def print_entries
-      visible_entries = @file_manager.entries[@offset + @start_line, @end_line - @start_line + 1]
+    # Print entries and return current position
+    def print_entries(entries, current_pos)
+      visible_entries = entries[@state.offset + @state.start_line, @state.end_line - @state.start_line + 1]
+      new_pos = nil
       visible_entries.each.with_index do |line, index|
-        if index == @current_line
-          @file_manager.current_pos = index + @offset
+        if index == @state.current_line
+          new_pos = index + @state.offset
           stdscr.attron(Curses.color_pair(1) | Curses::A_REVERSE) {
             stdscr.addstr("#{line}")
           }
@@ -77,16 +90,18 @@ module Navtor
       end
       stdscr.addstr("(empty)") if visible_entries.empty?
       stdscr.refresh
+
+      new_pos
     end
 
-    def status_line(str="", clear = true)
+    def status_line(entries, current_pos, clear = true)
       curx, cury = stdscr.curx, stdscr.cury
-      str = "#{Dir.pwd} (#{@file_manager.current_pos + 1}/#{@file_manager.entries.size}) off=#{@offset} cur=#{@current_line} end=#{@end_line} w=#{@cols} h=#{@lines} input=#{@input}" + str
+      str = "#{Dir.pwd} (#{current_pos + 1}/#{entries.size}) off=#{@state.offset} cur=#{@state.current_line} end=#{@state.end_line} w=#{@state.cols} h=#{@state.lines} input=#{@input}"
       if clear
-        stdscr.setpos(@lines - 1, 0)
-        stdscr.addstr(' ' * @cols)
+        stdscr.setpos(@state.lines - 1, 0)
+        stdscr.addstr(' ' * @state.cols)
       end
-      stdscr.setpos(@lines - 1, 0)
+      stdscr.setpos(@state.lines - 1, 0)
       stdscr.attron(Curses.color_pair(2) | Curses::A_NORMAL) {
         stdscr.addstr(str)
       }
@@ -96,36 +111,42 @@ module Navtor
 
     def run
       refresh
-      print_entries
-      status_line
+      new_pos = print_entries(@file_manager.entries, @file_manager.current_pos)
+      @file_manager.current_pos = new_pos unless new_pos.nil?
+      status_line(@file_manager.entries, @file_manager.current_pos)
       until (@input = stdscr.getch) == 'q'
         refresh
-        handle(@input)
-        print_entries
-        status_line
+        action = handle(@input, @file_manager.entries)
+        @file_manager.send(action) if action
+        new_pos = print_entries(@file_manager.entries, @file_manager.current_pos)
+        @file_manager.current_pos = new_pos unless new_pos.nil?
+        status_line(@file_manager.entries, @file_manager.current_pos)
       end
     ensure
       self.exit
     end
 
-    def handle(input)
+    # @return actions to be executed by file manager
+    def handle(input, entries)
       if input == 'j' || input == 258
-        @current_line = (@current_line == @end_line) ? @current_line : @current_line + 1
-        @offset += 1 if @current_line == @end_line && @offset + page_size < @file_manager.entries.size - 1
+        @state = @state.merge(current_line: (@state.current_line == @state.end_line) ? @state.current_line : @state.current_line + 1)
+        @state = @state.merge(offset: @state.offset + 1) if @state.current_line == @state.end_line && @state.offset + page_size < entries.size - 1
       elsif input == 'k' || input == 259
-        @current_line = (@current_line == @start_line) ? @current_line : @current_line - 1
-        @offset -=1 if @current_line == @start_line && @offset > 0
+        @state = @state.merge(current_line: (@state.current_line == @state.start_line) ? @state.current_line : @state.current_line - 1)
+        @state = @state.merge(offset: @state.offset - 1) if @state.current_line == @state.start_line && @state.offset > 0
       elsif input == 'g'
-        @offset = 0
-        @current_line = 0
+        @state = @state.merge(
+          offset: 0,
+          current_line: 0
+        )
       elsif input == 'G'
-        @offset = @file_manager.entries.size - 1 - page_size
-        @current_line = @end_line
+        @state = @state.merge(offset: entries.size - 1 - page_size)
+        @state = @state.merge(current_line: @state.end_line)
       elsif input == 'h' || input == 260
-        @offset = 0
-        @file_manager.to_parent_dir
+        @state = @state.merge(offset: 0)
+        :to_parent_dir
       elsif input == 'l' || input == 261
-        @file_manager.open_current
+        :open_current
       end
     end
 
